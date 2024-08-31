85addbaf0c33ee6d906636f48bd5c9d4dce206e9
shankarkr35 <shankarkr35@gmail.com>
2024-08-31 17:51:06 +0530

    Add API Of Properties

--- estate-src/real_estate/urls.py
+++ estate-src/real_estate/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('api/v1/auth/', include("djoser.urls")),  # Correct this line
     path('api/v1/auth/', include("djoser.urls.jwt")),  # Correct this line
     path('api/v1/profile/', include("apps.profiles.urls")),
+    path('api/v1/properties/', include("apps.properties.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
