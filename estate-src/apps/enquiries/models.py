9fe6b666fb0f819cd5112f617064614aec92d858
shankarkr35 <shankarkr35@gmail.com>
2024-08-31 15:23:33 +0530

    Add propery and enquiries

--- estate-src/apps/enquiries/models.py
+++ estate-src/apps/enquiries/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+from apps.common.models import TimeStampedUUIDModel
 
-# Create your models here.
+class Enquiry(TimeStampedUUIDModel):
+    name = models.CharField(_('Your Name'), max_length=250)
+    phone_number = PhoneNumberField(_('Phone Number'), max_length=30, default="+917717720891")
+    email = models.EmailField(_('Your Email'))
+    subject = models.CharField(_('Subject'), max_length=150)
+    message = models.TextField(_('Message'))
+
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        verbose_name_plural = "Enquiries"
---
+++ estate-src/apps/properties/exceptions.py
@@ -0,0 +1,4 @@
+from rest_framework.exceptions import APIException
+class PropertyNotFound(APIException):
+    status_code = 404
+    default_detail = "The requested property does not exists"
