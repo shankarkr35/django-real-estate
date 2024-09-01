e122edf0509d7a87c2256f9b733c8f64cbc1ce20
shankarkr35 <shankarkr35@gmail.com>
2024-09-01 07:55:31 +0530

    Add Curd Property

--- estate-src/apps/properties/serializers.py
+++ estate-src/apps/properties/serializers.py
@@ -11,8 +11,8 @@ class PropertySerializer(serializers.ModelSerializer):
         model = Property
         fields = ["id","user","title","slug","ref_code","description","country","city","postal_code","street_address","property_number","price","tax","final_property_price","plot_area","total_floors","bedrooms","bathrooms","advert_type","property_type","cover_photo","photo1","photo2","photo3","photo4","published_status","views"]
 
-        def get_user(self,obj):
-            return obj.user.username
+    def get_user(self,obj):
+        return obj.user.username
         
 class PropertyCreateSerializer(serializers.ModelSerializer):
     country = CountryField(name_only=True)
